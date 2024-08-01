df1478c6b03b124e36f01227b59ff07f9eda64dc
khemmasedth <khmasade555@gmail.com>
2024-08-01 18:19:06 +0700

    chellenge 3

--- main/main.cpp
+++ main/main.cpp
@@ -14,15 +14,38 @@ LED led8(23);
 
 LED leds[] = {led1, led2, led3, led4, led5, led6, led7, led8};
 
+// extern "C" void app_main(void)
+// {
+//     int i = 0;
+//     while (1)
+//     {
+//         leds[i].ON();
+//         vTaskDelay(100 / portTICK_PERIOD_MS);
+//         leds[i].OFF();
+//         if (i++ >= 7)
+//             i = 0;
+//     }
+// }
 extern "C" void app_main(void)
 {
-    int i = 0;
+    int patternIndex = 0;
+    int direction = 1;
+
     while (1)
     {
-        leds[i].ON();
+        for (int i = 0; i < 8; ++i)
+        {
+            leds[i].OFF();
+        }
+
+        leds[patternIndex].ON();
+
         vTaskDelay(100 / portTICK_PERIOD_MS);
-        leds[i].OFF();
-        if (i++ >= 7)
-            i = 0;
+
+        patternIndex += direction;
+        if (patternIndex == 7 || patternIndex == 0)
+        {
+            direction = -direction;
+        }
     }
 }
